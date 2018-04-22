1e3a01a098a0110a995a0532f6953edb8caed52b
guzhongren <guzhongren@live.cn>
2018-04-22 22:18:34 +0800

    fix(app.py):修复bug

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import flask
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "hello docker"
 
-    
+
 if __name__ == "__main__":
     app.run()
